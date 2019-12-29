122b3b84f5da087cceda2e202b5d3b7052ebd126
Christopher Bull <caffeinatedandroid@gmail.com>
2019-12-29 10:49:03 +0000

    Documentation: Util

--- util/index.js
+++ util/index.js
@@ -5,10 +5,12 @@ const { hostname } = require('os');
 
 module.exports = {
   /**
-   * Lookup the current devices' IP address by its hostname.
+   * Lookup the current device's IP address by its hostname.
    *
    * @param {(number|object)} [options] Optional options used by NodeJS's
    *  `dns.lookup(hostname[, options])`
+   * @returns {string} The current device's IP address.
+   *  Async function/Promise resolves to address.
    */
   getLocalIPAddress: async function getLocalIPAddress(options) {
     // Source: https://stackoverflow.com/a/55887284/508098
