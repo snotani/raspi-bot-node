2e1e4ea023c380f05316819da09b087850a79fbb
Christopher Bull <caffeinatedandroid@gmail.com>
2020-01-03 22:04:02 +0000

    Added additional tests

--- test/util/index.spec.ts
+++ test/util/index.spec.ts
@@ -2,9 +2,39 @@ import { getStrAsNumberOrDefault } from '../../src/util/primitves';
 import { expect } from 'chai';
 import 'mocha';
 
-describe('Str to Number util function', () => {
+describe('Util: Str to Number conversion', () => {
   it('should convert "10" to 10', () => {
     const result = getStrAsNumberOrDefault('10');
     expect(result).to.equal(10);
   });
+
+  const sNotANumber = 'word';
+  it(
+    `should fail to convert "${sNotANumber}" and ` +
+      'return predefined default (-1)',
+    () => {
+      const result = getStrAsNumberOrDefault(sNotANumber);
+      expect(result).to.equal(-1);
+    },
+  );
+
+  it(
+    'should fail to convert `undefined` and ' +
+      'return predefined default (-1)',
+    () => {
+      const result = getStrAsNumberOrDefault(undefined);
+      expect(result).to.equal(-1);
+    },
+  );
+
+  const sNotANumber2 = 'fail';
+  const defaultReturnValue = 123;
+  it(
+    `should fail to convert "${sNotANumber2}" and ` +
+      `return provided default (${defaultReturnValue})`,
+    () => {
+      const result = getStrAsNumberOrDefault(sNotANumber2, defaultReturnValue);
+      expect(result).to.equal(defaultReturnValue);
+    },
+  );
 });
